ec328fac3a576f69f70fc2800942eb03f40cc053
Bruno Sousa <bruno_brow7@hotmail.com>
2020-04-03 10:09:44 -0300

    Adicionando navbar e filtros e imagem para eventos

--- Agil-App/src/app/eventos/eventos.component.ts
+++ Agil-App/src/app/eventos/eventos.component.ts
@@ -7,13 +7,36 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./eventos.component.css']
 })
 export class EventosComponent implements OnInit {
+  _filtroLista: string;
+  get filtroLista(){
+    return this._filtroLista;
+  }  
+  set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
 
-  eventos: any;
+  eventosFiltrados: any [];
+  eventos: any = [];
+  imagemLargura = 50;
+  imagemMargem = 2;
+  mostrarImagem = false;
 
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
     this.getEventos();
+  }
+
+  alternarImagem() {
+    this.mostrarImagem = !this.mostrarImagem;
+  }
+
+  filtrarEventos(filtrarPor: string): any {
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      evento => evento.tema.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
   }
 
   getEventos(){
